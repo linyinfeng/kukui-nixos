8be23af2f16ec341567f310bfc56eb20e7d8d398
Noa Virellia <cmiki@amono.me>
2025-05-28 19:29:31 +0800

    feat: patches

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       url = "github:hexdump0815/kernel-config-options";
       flake = false;
     };
+    kernel-extra-patches = {
+      url = "github:hexdump0815/kernel-extra-patches";
+      flake = false;
+    };
   };
 
   outputs =
@@ -21,57 +25,76 @@
       flake-utils,
       mt81xx-kernel,
       kernel-config-options,
+      kernel-extra-patches,
     }:
     let
       pkgs = import nixpkgs {
         system = "aarch64-linux";
       };
+      lib = pkgs.lib;
+      listPatches =
+        path:
+        builtins.map
+          (_p: {
+            name = lib.strings.removeSuffix ".patch" _p;
+            patch = path + "/${_p}";
+          })
+          (
+            builtins.attrNames (
+              lib.attrsets.filterAttrs (path: type: type == "regular" && lib.strings.hasSuffix ".patch" path) (
+                builtins.readDir path
+              )
+            )
+          );
       mergeConfig =
         {
           kernelSrc,
           arch,
           extraConfigs,
         }:
-        pkgs.stdenv.mkDerivation {
-          pname = "merged-config";
-          version = "1.0.0";
-          src = kernelSrc;
-          dontConfigure = true;
-          buildPhase = ''
-            export ARCH=${arch}
-            scripts/kconfig/merge_config.sh -m arch/${arch}/configs/defconfig ${pkgs.lib.strings.concatStringsSep " " extraConfigs}
-          '';
-          installPhase = ''
-            mkdir $out
-            cp .config $out
-          '';
-        };
+        let
+          drv = pkgs.stdenvNoCC.mkDerivation {
+            pname = "merged-config";
+            version = "1.0.0";
+            src = kernelSrc;
+            dontConfigure = true;
+            buildPhase = ''
+              export ARCH=${arch}
+              scripts/kconfig/merge_config.sh -m arch/${arch}/configs/defconfig ${pkgs.lib.strings.concatStringsSep " " extraConfigs}
+            '';
+            installPhase = ''
+              mkdir $out
+              cp .config $out
+            '';
+          };
+        in
+        "${drv}/.config";
       kernelVer = "6.12.28";
       kernelSrc = pkgs.fetchurl {
         url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${kernelVer}.tar.xz";
         hash = "sha256-6KCZGCVirs/3gd5yznaUYecG2Xr0LXQN/yDrRQ3Vdx4=";
       };
-      config = mergeConfig {
-        inherit kernelSrc;
-        arch = "arm64";
-        extraConfigs = [
-          "${kernel-config-options}/chromebooks-aarch64.cfg"
-          "${kernel-config-options}/mediatek.cfg"
-          "${kernel-config-options}/docker-options.cfg"
-          "${kernel-config-options}/options-to-remove-generic.cfg"
-          "${mt81xx-kernel}/misc.cbm/options/options-to-remove-special.cfg"
-          "${kernel-config-options}/additional-options-generic.cfg"
-          "${kernel-config-options}/additional-options-aarch64.cfg"
-          "${mt81xx-kernel}/misc.cbm/options/additional-options-special.cfg"
-        ];
-      };
     in
     {
-      packages.aarch64-linux.kukui-kernel = pkgs.linuxPackages_custom {
+      packages.aarch64-linux.kukui-kernel = pkgs.linuxManualConfig {
         version = kernelVer;
         modDirVersion = "${kernelVer}-stb-cbm";
         src = kernelSrc;
-        configfile = "${config}/.config";
+        configfile = mergeConfig {
+          inherit kernelSrc;
+          arch = "arm64";
+          extraConfigs = [
+            "${kernel-config-options}/chromebooks-aarch64.cfg"
+            "${kernel-config-options}/mediatek.cfg"
+            "${kernel-config-options}/docker-options.cfg"
+            "${kernel-config-options}/options-to-remove-generic.cfg"
+            "${mt81xx-kernel}/misc.cbm/options/options-to-remove-special.cfg"
+            "${kernel-config-options}/additional-options-generic.cfg"
+            "${kernel-config-options}/additional-options-aarch64.cfg"
+            "${mt81xx-kernel}/misc.cbm/options/additional-options-special.cfg"
+          ];
+        };
+        kernelPatches = listPatches "${mt81xx-kernel}/misc.cbm/patches/v6.12";
       };
     }
     // flake-utils.lib.eachDefaultSystem (
