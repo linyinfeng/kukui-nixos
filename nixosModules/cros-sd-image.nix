28fe0b79e7c8c5b8ad22e66a90528c5fc0e68d41
Noa Virellia <cmiki@amono.me>
2025-05-29 17:30:22 +0800

    refactor: remove unused option

--- nixosModules/cros-sd-image.nix
+++ nixosModules/cros-sd-image.nix
@@ -32,16 +32,6 @@ in
       '';
     };
 
-    populateRootCommands = lib.mkOption {
-      example = lib.literalExpression "''\${config.boot.loader.generic-extlinux-compatible.populateCmd} -c \${config.system.build.toplevel} -d ./files/boot''";
-      description = ''
-        Shell commands to populate the ./files directory.
-        All files in that directory are copied to the
-        root (/) partition on the SD image. Use this to
-        populate the ./files/boot (/boot) directory.
-      '';
-    };
-
     compressImage = lib.mkOption {
       type = lib.types.bool;
       default = true;
